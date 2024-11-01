f9f4a478177bdd717cbfa46752a3e6e730babb6b
Angel Gala <galafloresangelemilio@gmail.com>
2024-11-01 08:50:20 -0500

    initial project

---
+++ src/components/Filters.jsx
@@ -0,0 +1,43 @@
+import { useId, useState } from "react";
+import "./Filters.css";
+
+export function Filters({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0);
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
+
+  const handleChangeMinPrice = (e) => {
+    setMinPrice(e.target.value);
+    onChange((prevState) => ({ ...prevState, minPrice: e.target.value }));
+  };
+
+  const handleChangeCategory = (e) => {
+    onChange((prevState) => ({ ...prevState, category: e.target.value }));
+  };
+
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor={minPriceFilterId}>Price</label>
+        <input
+          type="range"
+          id={minPriceFilterId}
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>{minPrice}</span>
+      </div>
+      <div>
+        <label htmlFor={categoryFilterId}>Categoría</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">Todos</option>
+          <option value="beauty">Beauty</option>
+          <option value="fragrances">Fragrances</option>
+          <option value="furniture">Furniture</option>
+          <option value="groceries">Groceries</option>
+        </select>
+      </div>
+    </section>
+  );
+}
